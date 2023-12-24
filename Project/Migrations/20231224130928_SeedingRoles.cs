e837bad853e17b7ef63cfc551c04e1189852a53f
Shukridozom <Shukridozom@hotmail.com>
2023-12-24 16:14:32 +0300

    Seeded roles to the database

---
+++ Project/Migrations/20231224130928_SeedingRoles.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Project.Models;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO Roles(Name) VALUES('{RoleName.Admin}')");
+            migrationBuilder.Sql($"INSERT INTO Roles(Name) VALUES('{RoleName.Author}')");
+            migrationBuilder.Sql($"INSERT INTO Roles(Name) VALUES('{RoleName.Student}')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM Roles");
+        }
+    }
+}
---
+++ Project/Models/RoleName.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Models
+{
+    public static class RoleName
+    {
+        public const string Admin = "Admin";
+        public const string Student = "Student";
+        public const string Author = "Author";
+    }
+}
