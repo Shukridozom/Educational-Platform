a49aa66febf2af7b75d389876ffd7b1d1ecc73d4
Shukridozom <Shukridozom@hotmail.com>
2023-12-24 14:50:08 +0300

    - Removed unnecessary models - Updated the database connection string - Tested database connection

--- Project/Models/User.cs
+++ Project/Models/User.cs
@@ -2,18 +2,11 @@
 {
     public class User
     {
-        public User()
-        {
-            Events = new List<Event>();
-            Participations = new List<Participation>();
-        }
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public IList<Event> Events { get; set; }
-        public IList<Participation> Participations { get; set; }
     }
 }
