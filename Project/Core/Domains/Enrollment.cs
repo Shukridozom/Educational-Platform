c4c1e4d8bc4ade24b81a3f72d3032a60abab5437
Shukridozom <Shukridozom@hotmail.com>
2024-01-05 22:53:31 +0300

    Added AdminPortionOfPrice to the enrollments table

--- Project/Core/Domains/Enrollment.cs
+++ Project/Core/Domains/Enrollment.cs
@@ -8,5 +8,6 @@
         public int CourseId { get; set; }
         public DateTime Date { get; set; }
         public double Price { get; set; }
+        public double AdminPortionOfPrice { get; set; }
     }
 }
---
+++ Project/Migrations/20240105191142_AddAdminPortionOfPriceToEnrollmentsTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminPortionOfPriceToEnrollmentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AdminPortionOfPrice",
+                table: "Enrollments",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdminPortionOfPrice",
+                table: "Enrollments");
+        }
+    }
+}
