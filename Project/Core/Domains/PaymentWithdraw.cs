ca943c75078df6ec0109a79d44a8028d11b0628b
Shukridozom <Shukridozom@hotmail.com>
2024-01-09 00:12:59 +0300

    Added amount column to PaymentWithdarw table

--- Project/Core/Domains/PaymentWithdraw.cs
+++ Project/Core/Domains/PaymentWithdraw.cs
@@ -8,6 +8,7 @@
         public User User { get; set; }
         public int UserId { get; set; }
         public DateTime Date { get; set; }
+        public double Amount { get; set; }
 
     }
 }
---
+++ Project/Migrations/20240108211210_AddAmountColumnToPaymentWithdrawTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAmountColumnToPaymentWithdrawTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Amount",
+                table: "PaymentWithdraw",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "PaymentWithdraw");
+        }
+    }
+}
