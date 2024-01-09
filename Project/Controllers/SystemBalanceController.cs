2fc173e5837a5ae7b375f8d4de6c515017616322
Shukridozom <Shukridozom@hotmail.com>
2024-01-10 00:06:35 +0300

    Created a controller for getting the balance and the profit of the system (by admins)

---
+++ Project/Controllers/SystemBalanceController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.Core;
+using Project.Core.Domains;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = RoleName.Admin)]
+    public class SystemBalanceController : AppControllerBase
+    {
+        public SystemBalanceController(IUnitOfWork unitOfWork, IConfiguration config, IMapper mapper)
+            : base(unitOfWork, config, mapper)
+        {
+
+
+        }
+        
+        [HttpGet("balance")]
+        public IActionResult GetBalance()
+        {
+            return Ok(unitOfWork.PaymentWithdraw.GetSystemBalance());
+        }
+
+        [HttpGet("profit")]
+        public IActionResult GetProfit()
+        {
+            return Ok(unitOfWork.Enrollments.GetSystemProfit());
+        }
+    }
+}
