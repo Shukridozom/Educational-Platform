7b784f2a1652455c128739ff5254164e86f6555b
Shukridozom <Shukridozom@hotmail.com>
2024-01-11 20:06:58 +0300

    Created a Stored Procedure for creating admin users directly from the database, the password hash has to be generated using this online tool: https://bcrypt.online/

---
+++ Project/Migrations/20240111163614_CreateStoredProcedureForCreatingAdminUsers.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateStoredProcedureForCreatingAdminUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+CREATE PROCEDURE CreateAdminUser(
+IN username VARCHAR(50), 
+IN email VARCHAR(255), 
+IN passwordHash VARCHAR(255), 
+IN firstName VARCHAR(255), 
+IN lastName VARCHAR(255))
+BEGIN
+INSERT INTO users (username, email, passwordHash, firstName, lastName, roleId)
+VALUES(username, email, passwordHash, firstName, lastName, (SELECT id FROM roles where name = 'Admin'));
+END;
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE IF EXISTS CreateAdminUser");
+        }
+    }
+}
