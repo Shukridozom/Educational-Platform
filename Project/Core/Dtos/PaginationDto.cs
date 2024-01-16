ae8c8508f337b859993612aea4d1cd6e0d574599
Shukridozom <Shukridozom@hotmail.com>
2024-01-16 05:54:47 +0300

    Created a DTO class for receiving pagination parameters [Note: this DTO should be received from the query string

---
+++ Project/Core/Dtos/PaginationDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.Core.Dtos
+{
+    public class PaginationDto
+    {
+        [Range(1, int.MaxValue)]
+        public int PageIndex { get; set; }
+        [Range(1, int.MaxValue)]
+        public int PageLength { get; set; }
+
+        public PaginationDto()
+        {
+            PageIndex = 1;
+            PageLength = 10;
+        }
+    }
+}
