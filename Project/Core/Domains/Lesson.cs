519e40901431f19d139d7c18b2a4e13b6f5c3cdf
Shukridozom <Shukridozom@hotmail.com>
2024-01-17 10:29:42 +0300

    Changed the data type of the  primary key of the  "Lessons" entity to byte (instead of int)

--- Project/Core/Domains/Lesson.cs
+++ Project/Core/Domains/Lesson.cs
@@ -2,10 +2,10 @@
 {
     public class Lesson
     {
-        public int Id { get; set; }
+        public byte Id { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
-        public byte Index { get; set; }
+        public int Index { get; set; }
         public Course Course { get; set; }
         public int CourseId { get; set; }
     }
---
+++ Project/Migrations/20240117072759_ChangeLessonsPrimarykeyDatatype.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLessonsPrimarykeyDatatype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Index",
+                table: "Lessons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint unsigned");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "Id",
+                table: "Lessons",
+                type: "tinyint unsigned",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Index",
+                table: "Lessons",
+                type: "tinyint unsigned",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Lessons",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint unsigned");
+        }
+    }
+}
