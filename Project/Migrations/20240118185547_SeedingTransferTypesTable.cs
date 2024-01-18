5d8b76df2d0d68d6ac53005f1ee644803b3f57fb
Shukridozom <Shukridozom@hotmail.com>
2024-01-18 22:22:14 +0300

    Fixed a bug: seeded TransferTypes table  (after being renamed from PaymentWithdrawTypes)

---
+++ Project/Migrations/20240118185547_SeedingTransferTypesTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Project.Core.Domains;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingTransferTypesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO TransferTypes (Name) VALUES ('{TransferTypeNames.Payment}')");
+            migrationBuilder.Sql($"INSERT INTO TransferTypes (Name) VALUES ('{TransferTypeNames.Withdraw}')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"DELETE FROM TransferTypes");
+        }
+    }
+}
